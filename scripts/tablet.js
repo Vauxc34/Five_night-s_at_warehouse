92f20dbc120f38587af6d82fa9f351ae03c1b52c
unknown <kaxowy12@gail.com>
2021-03-24 13:20:26 +0100

    update

--- scripts/tablet.js
+++ scripts/tablet.js
@@ -1,4 +1,6 @@
 const tablet = document.getElementById("tablet")
+const minutes_1 = document.querySelector("h1")
+const hours_1 = document.querySelector("h2")
 const btn_check = document.getElementById("check")
 const btn_doors = document.getElementById("door")
 
@@ -9,8 +11,12 @@ btn_check.addEventListener("click", () => {
     tablet.style.top = "0px"
     btn_check.style.zIndex = "-2"
     btn_doors.style.zIndex = "-2"
+    btn_check.style.opacity = "0"
+    btn_doors.style.opacity = "0"
+    minutes_1.style.zIndex = "-2"
+    hours_1.style.zIndex = "-2"
 
-    tablet.style.transition = "0.5s"
+    tablet.style.transition = "0.25s"
 
     /* */
 
@@ -20,6 +26,8 @@ btn_check.addEventListener("click", () => {
             tablet.style.top = "-1000px"
             btn_check.style.zIndex = "2"
             btn_doors.style.zIndex = "2"
+            btn_check.style.opacity = "1"
+            btn_doors.style.opacity = "1"
         }
     
     }
