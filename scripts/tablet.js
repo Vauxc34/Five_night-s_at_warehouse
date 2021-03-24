3aef1d39391812dcb6a51246f11d8d96cfab3806
unknown <kaxowy12@gail.com>
2021-03-24 10:31:29 +0100

    big-update

--- scripts/tablet.js
+++ scripts/tablet.js
@@ -6,86 +6,147 @@ const counter_four = document.getElementById("counter4")
 const tablet_frame = document.getElementById("tablet")
 let screen1 = document.getElementById("screen")
 
+const minimap = document.getElementById("minimap")
+const cam1 = document.getElementById("cam1")
+const cam2 = document.getElementById("cam4")
+const cam3 = document.getElementById("cam3")
+const cam4 = document.getElementById("cam2")
+
+const cam1_screen = document.getElementById("cam_screen1")
+const cam2_screen = document.getElementById("cam_screen4")
+const cam3_screen = document.getElementById("cam_screen3")
+const cam4_screen = document.getElementById("cam_screen2")
+
+cam1.addEventListener("click", () => {
+    minimap.style.backgroundImage = "url(/textures/mini_map_cam1.png)"
+})
+
+cam2.addEventListener("click", () => {
+    minimap.style.backgroundImage = "url(/textures/mini_map_cam2.png)"
+})
+
+cam3.addEventListener("click", () => {
+    minimap.style.backgroundImage = "url(/textures/mini_map_cam3.png)"
+})
+
+cam4.addEventListener("click", () => {
+    minimap.style.backgroundImage = "url(/textures/mini_map_cam4.png)"
+})
+
 const btn_show_hide = document.getElementById("check")
+const doors = document.getElementById("door")
 
 tablet_frame.style.top = "-1000px"
 
- btn_show_hide.addEventListener("click", () => {
+screen1.style.backgroundColor = 'green'
+
+btn_show_hide.addEventListener("click", () => {
 
     tablet_frame.style.top = "0px"
-    tablet_frame.style.transition = '2.5s'
-
-    /* const hide = () => {
-        tablet_frame.style.top = "-1000px"
-
-    }
-
-    setInterval(hide, 10000) */
+    tablet_frame.style.transition = '0.25s'
+    btn_show_hide.style.zIndex = "-1"
+    doors.style.zIndex = "-1"
 
 }) 
 
 
-
-screen1.style.backgroundColor = 'green'
-
-let actual_time_1 = 0
-let actual_time_2 = 0
-let actual_time_3 = 0
-let actual_time_4 = 0
-
-const passing_counter_one = () => {
-    actual_time_1 = Math.floor(Math.random() * 5)
-    counter_one.textContent = actual_time_1 + "counter_one"
-
-    if(actual_time_1 == 2) {
-        
-        screen1.style.backgroundColor = "red"
-
+window.addEventListener("keydown", () => {
+    if (event.keyCode === 27) {
+        tablet_frame.style.top = "-1000px"
+        btn_show_hide.style.zIndex = "2"
+        doors.style.zIndex = "2"
 
     }
+})
 
-}
+cam1.addEventListener("click", () =>{
 
-const passing_counter_two = () => {
-    actual_time_2 = Math.floor(Math.random() * 5)
-    counter_two.textContent = actual_time_2 + "counter_two"
+    let actual_time_1 = 0
 
-    if(actual_time_2 == 2) {
-        
-        screen1.style.backgroundColor = "green"
+    screen1.style.backgroundColor = 'purple'
 
+    const passing_counter_one = () => {
+        actual_time_1 = Math.floor(Math.random() * 5)
+        counter_one.textContent = actual_time_1 + "counter_one"
+    
+        if(actual_time_1 == 2) {
+            
+            screen1.style.backgroundColor = "Slateblue"
+    
+        } if(actual_time_1 == 1) {
 
+            screen1.style.backgroundColor = "purple"
+
+        } 
+    
     }
 
-}
+    setInterval(passing_counter_one, 1000)
 
-const passing_counter_three = () => {
-    actual_time_3 = Math.floor(Math.random() * 5)
-    counter_three.textContent = actual_time_3 + "counter_three"
+})
 
-    if(actual_time_3 == 2) {
-        
-        screen1.style.backgroundColor = "yellow"
+cam2.addEventListener("click", () =>{
 
 
+    let actual_time_2 = 0
+
+    screen1.style.backgroundColor = 'orange'
+
+    const passing_counter_two = () => {
+        actual_time_2 = Math.floor(Math.random() * 5)
+        counter_two.textContent = actual_time_2 + "counter_two"
+    
+        if(actual_time_2 == 4) {
+            
+            screen1.style.backgroundColor = "tomato"
+    
+    
+        }
+    
     }
 
-}
+    setInterval(passing_counter_two, 1000)
 
-const passing_counter_four = () => {
-    actual_time_4 = Math.floor(Math.random() * 5)
-    counter_four.textContent = actual_time_4 + "counter_four"
+})
 
-    if(actual_time_4 == 2) {
-        
-        screen1.style.backgroundColor = "blue"
+cam3.addEventListener("click", () =>{
 
+    let actual_time_3 = 0
 
+    screen1.style.backgroundColor = 'blue'
+
+    const passing_counter_three = () => {
+        actual_time_3 = Math.floor(Math.random() * 5)
+        counter_three.textContent = actual_time_3 + "counter_three"
+        if(actual_time_3 == 1) {
+            
+            screen1.style.backgroundColor = "aquamarine"
+    
+        }
     }
 
-}
+    setInterval(passing_counter_three, 1000)
 
-setInterval(passing_counter_one, 1000)
-setInterval(passing_counter_two, 1000)
-setInterval(passing_counter_three, 1000)
-setInterval(passing_counter_four, 1000)
+
+})
+
+cam4.addEventListener("click", () =>{
+
+    let actual_time_4 = 0
+
+    screen1.style.backgroundColor = 'white'
+
+    const passing_counter_four = () => {
+        actual_time_4 = Math.floor(Math.random() * 5)
+        counter_four.textContent = actual_time_4 + "counter_four"
+        if(actual_time_4 == 1) {
+            
+            screen1.style.backgroundColor = "whitesmoke"
+    
+        }
+    }
+
+    setInterval(passing_counter_four, 1000)
+
+
+})
